d12994c73f3b98761b17c9e9dee0a6181dda07ad
Arunpraveen98 <arunfbfb78@gmail.com>
2023-02-06 20:02:36 +0530

    Keyword updated

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-var description = document.createElement("p");
+let description = document.createElement("p");
 description.setAttribute("id", "description");
 description.innerText =
   "Pagination is the method of separating digital content into different pages on a website. Users can navigate between these pages by clicking below Buttons.";
 
 //----------------------------------------------------------------
 
-var container = ele("div", "class", "container");
-var maindiv = ele("div", "id", "main-div");
-var navbar = ele("nav", "id", "nav-bar");
-var title = ele("h1", "id", "title");
+let container = ele("div", "class", "container");
+let maindiv = ele("div", "id", "main-div");
+let navbar = ele("nav", "id", "nav-bar");
+let title = ele("h1", "id", "title");
 title.innerHTML = "DOM-Pagination task :";
 
 function ele(tagname, attname, attvalue) {
-  var element = document.createElement(tagname);
+  let element = document.createElement(tagname);
   element.setAttribute(attname, attvalue);
   return element;
 }
 
 //----------------------------------------------------------------
-var content_div = document.createElement("div");
+let content_div = document.createElement("div");
 content_div.setAttribute("id", "buttons");
 content_div.setAttribute("class", "d-flex justify-content-center");
 
 //----------------------------------------------------------------
 
-var ul_list = document.createElement("ul");
+let ul_list = document.createElement("ul");
 ul_list.classList.add("pagination", "justify-content-center");
 ul_list.setAttribute("id", "ul-list");
 
 //----------------------------------------------------------------
 
-var list_pre = lists("li");
-var list_first = lists("li");
+let list_pre = lists("li");
+let list_first = lists("li");
 //--
-var list_1 = lists("li");
-var list_2 = lists("li");
-var list_3 = lists("li");
-var list_4 = lists("li");
-var list_5 = lists("li");
-var list_6 = lists("li");
+let list_1 = lists("li");
+let list_2 = lists("li");
+let list_3 = lists("li");
+let list_4 = lists("li");
+let list_5 = lists("li");
+let list_6 = lists("li");
 //--
-var list_last = lists("li");
-var list_next = lists("li");
+let list_last = lists("li");
+let list_next = lists("li");
 
 function lists(tagname) {
-  var lists = document.createElement(tagname);
+  let lists = document.createElement(tagname);
   return lists;
 }
 
 //----------------------------------------------------------------
 
-var btn_pre = btnele(
+let btn_pre = btnele(
   "button",
   "type",
   "button",
@@ -60,7 +60,7 @@ var btn_pre = btnele(
   "event-btn-pre",
   "⏪Previous"
 );
-var btn_first = btnele(
+let btn_first = btnele(
   "button",
   "type",
   "button",
@@ -71,7 +71,7 @@ var btn_first = btnele(
   "First"
 );
 
-var btn_last = btnele(
+let btn_last = btnele(
   "button",
   "type",
   "button",
@@ -81,7 +81,7 @@ var btn_last = btnele(
   "event-btn-last",
   "Last"
 );
-var btn_next = btnele(
+let btn_next = btnele(
   "button",
   "type",
   "button",
@@ -92,7 +92,7 @@ var btn_next = btnele(
   "Next⏩"
 );
 
-var btn_1 = btnele(
+let btn_1 = btnele(
   "button",
   "type",
   "button",
@@ -102,7 +102,7 @@ var btn_1 = btnele(
   "event-btn-1",
   "1"
 );
-var btn_2 = btnele(
+let btn_2 = btnele(
   "button",
   "type",
   "button",
@@ -112,7 +112,7 @@ var btn_2 = btnele(
   "event-btn-2",
   "2"
 );
-var btn_3 = btnele(
+let btn_3 = btnele(
   "button",
   "type",
   "button",
@@ -122,7 +122,7 @@ var btn_3 = btnele(
   "event-btn-3",
   "3"
 );
-var btn_4 = btnele(
+let btn_4 = btnele(
   "button",
   "type",
   "button",
@@ -132,7 +132,7 @@ var btn_4 = btnele(
   "event-btn-4",
   "4"
 );
-var btn_5 = btnele(
+let btn_5 = btnele(
   "button",
   "type",
   "button",
@@ -142,7 +142,7 @@ var btn_5 = btnele(
   "event-btn-5",
   "5"
 );
-var btn_6 = btnele(
+let btn_6 = btnele(
   "button",
   "type",
   "button",
@@ -163,7 +163,7 @@ function btnele(
   attvalue2,
   contenttext
 ) {
-  var button = document.createElement(tagname);
+  let button = document.createElement(tagname);
   button.setAttribute(attname1, attvalue1);
   button.classList.add(class1, class2);
   button.setAttribute(attname2, attvalue2);
@@ -173,301 +173,167 @@ function btnele(
 
 //----------------------------------------------------------------
 
-var table_div = createtable("div");
+let table_div = createtable("div");
 table_div.setAttribute("class", "table-responsive");
 
-var table_tag = createtable("table");
+let table_tag = createtable("table");
 table_tag.classList.add("table", "table-bordered");
 
-var table_body = createtable("tbody");
+let table_body = createtable("tbody");
 table_body.setAttribute("id", "t-body");
 
-var table_head = createtable("thead");
+let table_head = createtable("thead");
 table_head.setAttribute("id", "table-head");
-var table_tr = createtable("tr");
+let table_tr = createtable("tr");
 
-var table_th1 = createtable("th");
+let table_th1 = createtable("th");
 table_th1.innerText = "Id";
 
-var table_th2 = createtable("th");
+let table_th2 = createtable("th");
 table_th2.innerText = "Name";
 
-var table_th3 = createtable("th");
+let table_th3 = createtable("th");
 table_th3.innerText = "Email";
 
 function createtable(taganme) {
-  var elements = document.createElement(taganme);
+  let elements = document.createElement(taganme);
   return elements;
 }
 //--------------------------------------------------------------
 
 paginationData();
 async function paginationData() {
-  var res = await fetch(
+  let res = await fetch(
     "https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json"
   );
-  var JsonFormat = await res.json();
+  let JsonFormat = await res.json();
   // console.log(JsonFormat);
 
   try {
-    var namearr = [];
-    var emailarr = [];
-    var idarr = [];
-    for (var i = 0; i < 100; i++) {
-      var name = JsonFormat[i].name;
+    let namearr = [];
+    let emailarr = [];
+    let idarr = [];
+    for (let i = 0; i < 100; i++) {
+      let name = JsonFormat[i].name;
       namearr.push(name);
       // console.log(name);
-      var Email = JsonFormat[i].email;
+      let Email = JsonFormat[i].email;
       emailarr.push(Email);
       // console.log(Email);
-      var id = JsonFormat[i].id;
+      let id = JsonFormat[i].id;
       idarr.push(id);
       // console.log(id);
     }
 
     //----------------------------------------------------------------
-    //First-button:-
 
-    var addEvent = document.getElementById("event-btn-first");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      for (var k = 0; k < 6; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
+    //*First button initial value...
+    let starting_value = 0;
+
+    //? Previous Button...
+    let addEvent_pre = document.getElementById("event-btn-pre");
+    addEvent_pre.addEventListener("click", () => {
+      if (starting_value > 0) {
+        starting_value = starting_value - 5;
+        display_contents(starting_value);
+      } else {
+        alert("You are already at the beginning of the First page!");
+      }
+    });
+    //----------------------------------------------------------------
+    //? Next Button...
+    let addEvent_next = document.getElementById("event-btn-next");
+    addEvent_next.addEventListener("click", () => {
+      if (starting_value < 30 && starting_value >= 0) {
+        starting_value = starting_value + 5;
+        display_contents(starting_value);
+      } else {
+        alert("You are already at the Last page!");
+      }
+    });
+    //----------------------------------------------------------------
+    //? First button...
+    let addEvent_first = document.getElementById("event-btn-first");
+    addEvent_first.addEventListener("click", () => {
+      starting_value = 0;
+      display_contents(starting_value);
+    });
+    // ----------------------------------------------------------------
+    //? 2nd button...
+    let addEvent_2nd = document.getElementById("event-btn-2");
+    addEvent_2nd.addEventListener("click", () => {
+      starting_value = 5;
+      display_contents(starting_value);
+    });
+    // ----------------------------------------------------------------
+    //? 3rd button...
+    let addEvent_3rd = document.getElementById("event-btn-3");
+    addEvent_3rd.addEventListener("click", () => {
+      starting_value = 10;
+      display_contents(starting_value);
+    });
+    // ----------------------------------------------------------------
+    //? 4th button...
+    let addEvent_4th = document.getElementById("event-btn-4");
+    addEvent_4th.addEventListener("click", () => {
+      starting_value = 15;
+      display_contents(starting_value);
+    });
+    // ----------------------------------------------------------------
+    //? 5th button...
+    let addEvent_5th = document.getElementById("event-btn-5");
+    addEvent_5th.addEventListener("click", () => {
+      starting_value = 20;
+      display_contents(starting_value);
+    });
+    // ----------------------------------------------------------------
+    //? 6th button...
+    let addEvent_6th = document.getElementById("event-btn-6");
+    addEvent_6th.addEventListener("click", () => {
+      starting_value = 25;
+      display_contents(starting_value);
+    });
+    // ----------------------------------------------------------------
+    //? Last button...
+    let count = 0;
+    let addEvent_last = document.getElementById("event-btn-last");
+    addEvent_last.addEventListener("click", () => {
+      count++;
+      starting_value = 30;
+      display_contents(starting_value);
+      if (starting_value === 25 || count >= 2) {
+        alert("You are already at the end of the page!");
+      }
+    });
+
+    // ----------------------------------------------------------------
+
+    function display_contents(Number_of_items) {
+      let Table_body = document.getElementById("t-body");
+      Table_body.innerHTML = "";
+
+      for (let i = Number_of_items; i < Number_of_items + 5; i++) {
+        eventButtonFirst(`${idarr[i]}`, `${emailarr[i]}`, `${namearr[i]}`);
+
         function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
+          let row = document.createElement("tr");
+          let td1 = document.createElement("td");
           td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
+          let td2 = document.createElement("td");
           td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
+          let td3 = document.createElement("td");
           td3.innerHTML = `${nameData}`;
           table_body.append(row);
           row.append(td1, td2, td3);
         }
       }
-    });
+    }
 
-  //----------------------------------------------------------------
-  //1-button:-
-    var addEvent = document.getElementById("event-btn-1");
-    addEvent.addEventListener("click", () => {
-      var a1=document.getElementById("t-body");
-      a1.innerHTML="";
-      
-      for (var k = 6; k < 12; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
+    display_contents(starting_value);
 
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
+    //----------------------------------------------------------------
 
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-     } 
-    });
-
-  //----------------------------------------------------------------
-  //2-button:-
-    var addEvent = document.getElementById("event-btn-2");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      
-      for (var k = 12; k < 18; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
-
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
-
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-     }    
-    });
-
-  //----------------------------------------------------------------
-  //3-button:-
-    var addEvent = document.getElementById("event-btn-3");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      
-      for (var k = 18; k < 24; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
-
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
-
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-      }
-    });
-
-  //----------------------------------------------------------------
-  //4-button:-
-    var addEvent = document.getElementById("event-btn-4");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      
-      for (var k = 24; k < 30; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
-
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
-
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-      }
-    });
-
-  //----------------------------------------------------------------
-  //5-button:-
-    var addEvent = document.getElementById("event-btn-5");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      
-      for (var k = 30; k < 36; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
-
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
-
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-      }
-    });
-
-  //----------------------------------------------------------------
-  //6-button:-
-    var addEvent = document.getElementById("event-btn-6");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      
-      for (var k = 36; k < 42; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
-
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
-
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-      }
-    });
-
-  //----------------------------------------------------------------
-  //Next-button:-
-    var addEvent = document.getElementById("event-btn-next");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      
-      for (var k = 42; k < 48; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
-
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
-
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-      }
-    });
-  
-  //----------------------------------------------------------------
-  //Last-button:-
-    var addEvent = document.getElementById("event-btn-last");
-    addEvent.addEventListener("click", () => {
-      var a=document.getElementById("t-body");
-      a.innerHTML="";
-      
-      for (var k = 48; k < 54; k++) {
-        eventButtonFirst(`${idarr[k]}`, `${emailarr[k]}`, `${namearr[k]}`);
-        function eventButtonFirst(idData, emailData, nameData) {
-          var row = document.createElement("tr");
-          var td1 = document.createElement("td");
-
-          td1.innerHTML = `${idData}`;
-          var td2 = document.createElement("td");
-
-          td2.innerHTML = `${emailData}`;
-          var td3 = document.createElement("td");
-
-          td3.innerHTML = `${nameData}`;
-
-          table_body.append(row);
-          row.append(td1, td2, td3);
-        }
-      }
-    });
-
-   //----------------------------------------------------------------
-
-   //catch-block:-
-
+    //? catch-block:-
   } catch (error) {
     console.log(error);
   }
@@ -501,7 +367,6 @@ list_pre.append(btn_pre);
 
 list_first.append(btn_first);
 
-list_1.appendChild(btn_1);
 list_2.appendChild(btn_2);
 list_3.appendChild(btn_3);
 list_4.appendChild(btn_4);
@@ -519,6 +384,5 @@ table_tag.append(table_head, table_body);
 table_head.append(table_tr);
 
 table_tr.append(table_th1, table_th2, table_th3);
-
 
 //----------------------------------------------------------------
